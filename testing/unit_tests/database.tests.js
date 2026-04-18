ad0d610c4b8ab9a25b7c6eda0cff13bbe87e8eb0
alat414 <gustavo.alatriste@g.austincc.edu>
2026-04-18 15:54:43 -0500

    renaming tokenutiltests to databasetests; adding comment block to file; updated html coverage files

---
+++ testing/unit_tests/database.tests.js
@@ -0,0 +1,10 @@
+/* *************************************************
+*  Name: Gustavo Alatriste
+*  Assignment: Project One JWKS server
+*  Purpose: Database testing functions
+*           using functions and calls to ensure
+*           database actions including opening,
+*           creating, and connections are working
+*           correctly. 
+*           database.tests.js
+************************************************* */
